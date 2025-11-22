c72fe4f34d9e1818a4535eb45c39ea53c75f3793
Ramuul <raalmdd@gmail.com>
2025-11-22 09:10:31 -0500

    1

--- ppmonk/core/__init__.py
+++ ppmonk/core/__init__.py
@@ -1,6 +1,6 @@
 """Core combat logic for PPMonk."""
 
-from .player import Player
+from .player import PlayerState as Player  # [修复] 导入 PlayerState 并别名为 Player
 from .spell_book import SpellBook
 from .buff_manager import BuffManager
 from .timeline import Timeline
